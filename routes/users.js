9ee82abd404c77d05755db478b3e4edc0e483de4
Kate <54095868+romcath@users.noreply.github.com>
2020-03-11 21:16:55 +0300

    Changed route for /users

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,18 @@
 const route = require('express').Router();
 const users = require('../data/users.json');
 
-const doesUserExist = (req, res, next) => {
+route.get('/users/:_id', (req, res) => {
   const { _id } = req.params;
   const user = users.find(item => item._id === _id);
 
   if (!user) {
     res.status(404).send({ "message": "Нет пользователя с таким id" });
-    return;
-  }
-  next();
-};
+  } else res.send(user);
+});
 
-const sendUserId = (req, res) => {
-  res.send({ "_id": req.params._id });
-}
 
-const sendUsers = (req, res) => {
+route.get('/users', (req, res) => {
   res.send(users);
-};
-
-route.get('/users/:_id', doesUserExist);
-route.get('/users/:_id', sendUserId);
-route.get('/users', sendUsers);
+});
 
 module.exports = route;
