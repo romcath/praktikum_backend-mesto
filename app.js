f370272f57ee36d4aa6adc7b1ee335e28e7929d4
Kate <54095868+romcath@users.noreply.github.com>
2020-03-11 11:50:48 +0300

    Created route for /cards

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const routeCards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routeCards);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
