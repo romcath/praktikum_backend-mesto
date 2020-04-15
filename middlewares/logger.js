03a54a9c7b69968abaaff088da4a18570ff1a366
Kate <54095868+romcath@users.noreply.github.com>
2020-04-15 18:59:02 +0300

    Fixed eslint errors

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -2,15 +2,15 @@ const winston = require('winston');
 const expressWinston = require('express-winston');
 
 const requestLogger = expressWinston.logger({
-    transports: [
-        new winston.transports.File({ filename: 'request.log' }),
-    ],
-    format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'request.log' }),
+  ],
+  format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-      new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: 'error.log' }),
   ],
   format: winston.format.json(),
 });
