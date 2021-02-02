33c50780e904617cf6b6fce139aaa4be328584a8
Kate <54095868+romcath@users.noreply.github.com>
2021-02-02 19:40:53 +0300

    fix: update cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 
 const errorHandler = require('./middlewares/error-handler');
 const routes = require('./routes/index');
@@ -13,37 +13,18 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-const allowedCors = [
-  'http://mesto.cf',
-  'http://mesto.cf/auth.html',
-  'http://api.mesto.cf',
-  'http://localhost:8080'
-];
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-});
-
-// app.use(cors(({
-//   origin: [
-//     'http://localhost:8080',
-//     'http://mesto.cf/',
-//     'http://api.mesto.cf',
-//     'https://mesto.cf',
-//     'https://romcath.github.io',
-//   ],
-//   methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-//   credentials: true,
-// })));
+app.use(cors(({
+  origin: [
+    'http://localhost:8080',
+    'http://mesto.cf/',
+    'http://mesto.cf/auth.html',
+    'http://api.mesto.cf',
+    'https://mesto.cf',
+    'https://romcath.github.io',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+})));
 
 app.use(cookieParser());
 
