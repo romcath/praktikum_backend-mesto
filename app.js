e5bc81cecfe313f71e3ecc500f5dc7e4c1743fc3
Kate <54095868+romcath@users.noreply.github.com>
2021-02-02 19:07:52 +0300

    fix: update cors

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(function(req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', origin);
   }
 
